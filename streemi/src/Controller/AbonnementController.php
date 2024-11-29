9e07b232dfa1e01f18d6f29f66300b6b21987d2c
Mak Ludovic <ludovic93mak@gmail.com>
2024-11-29 12:00:00 +0100

    save

--- streemi/src/Controller/AbonnementController.php
+++ streemi/src/Controller/AbonnementController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\SubscriptionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,14 @@ use Symfony\Component\Routing\Attribute\Route;
 class AbonnementController extends AbstractController
 {
     #[Route('/abonnement', name: 'app_abonnement')]
-    public function index(): Response
+    public function index(
+        SubscriptionRepository $subscriptionRepository
+    ): Response
     {
+        $subscriptions = $subscriptionRepository->findAll();
         return $this->render('abonnement/abonnements.html.twig', [
             'controller_name' => 'AbonnementController',
+            'subscriptions'=> $subscriptions
         ]);
     }
 }
--- streemi/src/Controller/MovieController.php
+++ streemi/src/Controller/MovieController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
+use App\Repository\MovieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -19,18 +21,24 @@ class MovieController extends AbstractController
         return $this->render('movie/detail.html.twig');
     }
     #[Route('/movie/discover', name: 'app_movie_discover')]
-    public function discover(): Response
+    public function discover(
+        CategoryRepository $categoryRepository
+    ): Response
     {
-        return $this->render('movie/discover.html.twig');
+        $categories = $categoryRepository->findAll();
+        return $this->render('movie/discover.html.twig',['categories' => $categories]);
     }
     #[Route('/', name: 'app_movie')]
-    public function index(): Response
+    public function index(
+        MovieRepository $movieRepository
+    ): Response
     {
-        return $this->render('movie/index.html.twig');
+        $movies = $movieRepository->findAll();
+        return $this->render('movie/index.html.twig', ['movies' => $movies]);
     }
     #[Route('/movie/list', name: 'app_movie_list')]
     public function list(): Response
     {
-        return $this->render('movie/list.html.twig');
+        return $this->render('movie/lists.html.twig');
     }
 }
